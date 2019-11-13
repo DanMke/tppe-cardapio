b7bc7eac726f69e8c729b20e7859f62b2ff0f4ac
Joberth Rogers <joberth.rogers18@gmail.com>
2019-11-12 22:27:17 -0300

    Parametrizando e triangulando os testes referente ao Alimento Controller
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- src/tests/AlimentoControllerTest.java
+++ src/tests/AlimentoControllerTest.java
@@ -2,7 +2,13 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import exceptions.DadoIncompletoException;
 import models.Alimento;
@@ -10,48 +16,57 @@ import models.Grupo;
 import controllers.AlimentoController;
 import controllers.GrupoController;
 
+@RunWith(Parameterized.class)
 public class AlimentoControllerTest {
 	
-	@Test
-	public void testEditarAlimento() throws DadoIncompletoException{
-		
-		String nome = "Macarrão";
-		String novoNome = "Frango";
-		String medida = "10 g";
-		Grupo gp = new Grupo("Massa");
-		GrupoController.salvarNaLista(gp);
-		Grupo gp2 = new Grupo("Carnes e Ovos");
-		GrupoController.salvarNaLista(gp2);
-		
-		Alimento al1 = new Alimento(nome, medida, gp);
-		AlimentoController.salvarNaLista(al1);
-		Alimento al2 = AlimentoController.editarAlimento(nome, novoNome, medida, gp2.getNome());
-		
-		assertEquals(novoNome, al2.getNome());
-		assertEquals(medida, al2.getMedida());
-		assertEquals(gp2.getNome(), al2.getGrupo().getNome());
-		
-	}
+	@Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {     
+                 { "Vegetais", "Brocolis", "10 porcoes", "Carnes e Ovos", "Bisteca", "30 gramas"},
+                 { "Massa", "Macarrão", "10 gramas", "Carboidrato", "Arroz", "5 gramas"},
+                 { "Verde", "Couve", "20 gramas", "Carnes e Ovos", "Costela", "7 gramas"}
+           });
+    }
+    
+    private Grupo gp;
+    private Grupo gp2;
+    private Alimento al;
+    private String newNomeGrupo;
+    private String newMedida;
+    private String newNomeAlimento;
+    private Alimento newAlimento;
+    
+    
+    public AlimentoControllerTest(String oldNomeGrupo, String oldNomeAlimento, String oldMedida, String newNomeGrupo, String newNomeAlimento, String newMedida ) throws DadoIncompletoException {
+    	this.gp = new Grupo(oldNomeGrupo);
+    	GrupoController.salvarNaLista(gp);
+    	
+    	this.gp2 = new Grupo(newNomeGrupo);
+    	GrupoController.salvarNaLista(gp2);
+    	
+    	this.al = new Alimento(oldNomeAlimento, oldMedida, gp);
+    	AlimentoController.salvarNaLista(al);
+    	
+    	this.newNomeAlimento = newNomeAlimento;
+    	this.newMedida = newMedida;
+    	this.newNomeGrupo = newNomeGrupo;
+    	
+    	this.newAlimento = AlimentoController.editarAlimento(oldNomeAlimento, newNomeAlimento, newMedida, newNomeGrupo);
+    }
+    
+    @Test
+    public void testeEditarNomeAlimento() {
+    	assertEquals(this.newNomeAlimento, this.newAlimento.getNome());
+    }
+    
+    @Test
+    public void testeEditarMedidaAlimento() {
+    	assertEquals(this.newMedida, this.newAlimento.getMedida());
+    }
+    
+    @Test
+    public void testeEditarGrupoAlimento() {
+    	assertEquals(this.newNomeGrupo, this.newAlimento.getGrupo().getNome());
+    }
 	
-	@Test
-	public void testEditarAlimento2() throws DadoIncompletoException {
-		
-		String nome = "Brocolis";
-		String novoNome = "Arroz";
-		String medida = "30 g";
-		String novaMedida = "10 g";
-		Grupo gp = new Grupo("Vegetais");
-		Grupo gp2 = new Grupo("Carboidratos");
-		GrupoController.salvarNaLista(gp2);
-		
-		Alimento al1 = new Alimento(nome, medida, gp);
-		AlimentoController.salvarNaLista(al1);
-		Alimento al2 = AlimentoController.editarAlimento(nome, novoNome, novaMedida, gp2.getNome());
-		
-		assertEquals(novoNome, al2.getNome());
-		assertEquals(novaMedida, al2.getMedida());
-		assertEquals(gp2.getNome(), al2.getGrupo().getNome());
-		
-	}
-
 }
