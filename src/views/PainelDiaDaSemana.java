cc1862dea3e4af81af4fac5be48caf4b525887bd
Gabriela <gabriela.medeiros2010@hotmail.com.br>
2019-11-13 05:17:43 -0200

    Pegando dados reais

--- src/views/PainelDiaDaSemana.java
+++ src/views/PainelDiaDaSemana.java
@@ -7,12 +7,18 @@ import javax.swing.JButton;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JLabel;
 import java.awt.Component;
 import java.awt.Dimension;
 import javax.swing.border.LineBorder;
+
+import controllers.GrupoController;
+import exceptions.DadoIncompletoException;
+import models.Grupo;
+
 import java.awt.Color;
 
 @SuppressWarnings("serial")
@@ -25,7 +31,7 @@ public class PainelDiaDaSemana extends JPanel implements ActionListener {
 	private JComboBox comboBox;
 	private JButton btnAdicionar;
 	private JPanel panelAdicionados;
-	private List<String> listaGrupos;
+	private List<Grupo> listaGrupos;
 
 	public PainelDiaDaSemana(String dia) {
 		setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -43,15 +49,15 @@ public class PainelDiaDaSemana extends JPanel implements ActionListener {
 		lblDia = new JLabel(dia);
 		lblDia.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
+		listaGrupos = new ArrayList();
+		
 		panelAdicionar = new JPanel();
 		flowLayout = (FlowLayout) panelAdicionar.getLayout();
-		flowLayout.setVgap(10);
-		flowLayout.setHgap(50);
+//		flowLayout.setVgap(10);
+//		flowLayout.setHgap(50);
 		panelAdicionar.setMaximumSize(new Dimension(300, 300));
 		
-		// TODO fazer método que retorne uma String[] com todos os nomes de grupos
-		String[] grupos = {"Carboidrato", "Proteína"};
-		comboBox = new JComboBox(grupos);
+		comboBox = new JComboBox(GrupoController.getNomesGrupos());
 		
 		btnAdicionar = new JButton("+");
 		btnAdicionar.addActionListener(this);
@@ -69,9 +75,15 @@ public class PainelDiaDaSemana extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO criar método que envie nome String e retorne o grupo
+
 		String nomeGrupo = comboBox.getSelectedItem().toString();
-		//listaGrupos.add(Grupo.getGrupo(nomeGrupo));
+		try {
+			listaGrupos.add(GrupoController.obterGrupo(nomeGrupo));
+		} catch (DadoIncompletoException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		panelAdicionados.add(new JLabel(nomeGrupo));
 		panelAdicionados.validate();
 		panelAdicionados.repaint();		
